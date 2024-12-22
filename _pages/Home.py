c46c5f7bed6185129b2cea47a0742a01343b8359
haiouxiang <haiouxiang@gmail.com>
2024-12-22 11:21:31 -0800

    fix images

--- _pages/Home.py
+++ _pages/Home.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.header("Welcome to My Portfolio")
 st.write("Hi! I'm Archer Xiang. This is my personal portfolio where I share my projects and interests.")
-st.image("./imgs/archer_about.jpg", caption="That's me!", width=300)
+st.image(r"imgs/archer_about.jpg", caption="That's me!", width=300)
